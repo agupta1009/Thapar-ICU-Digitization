b22381e3f17ca686fb03ab9ebfc7f2b6cb537060
agupta1009 <67683002+agupta1009@users.noreply.github.com>
2020-09-17 16:32:52 +0530

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,7 +1,30 @@
+import pyttsx3 as p
 import speech_recognition as sr
 from utils import recognize_speech_from_mic
+engine=p.init()
+engine.setProperty('rate',200)
+engine.setProperty('volume',1.0)
+# will pick the keys from the data base
+record={
+    "patient Name":[],
+    "patient ID": [],
+    "patient Age":[],
+    "doctor handling the case":[],
+    "symptoms":[],
+    "medicine":[],
+}
 r = sr.Recognizer()
 m = sr.Microphone()
-print("-----------------START SPEAKING------------------")
-response=recognize_speech_from_mic(r, m)  # speak after running this line
-print(response)
+for keys in record:
+    # computer will speak to give the output
+    p.speak(f" please tell the {keys}")
+    print(f" please tell the {keys}")
+    # transcription received
+    text=recognize_speech_from_mic(r, m)
+    print(text)
+    record[keys].append(text)
+
+# here the records are printed can be directly saved to database
+for keys in record:
+     print(f"{keys} : {record[keys]}")
+    
