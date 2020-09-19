5981da18222e9bad4217b68787da5440902ecbf5
Ansh Garg <anshgarg7@gmail.com>
2020-09-20 00:51:17 +0530

    Data able to be sent to the database

--- stt.py
+++ stt.py
@@ -1,15 +1,23 @@
 import speech_recognition as sr
 from utils import recognize_speech_from_mic
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 r = sr.Recognizer()
 m = sr.Microphone()
 print("-----------------START SPEAKING------------------")
 response=recognize_speech_from_mic(r, m)  # speak after running this line
 print(response)
-return_tuple = (response["transcription"],response["success"],response["error"])
+test = []
+test.append(response["transcription"])
+test.append(response["success"])
+test.append(response["error"])
+return_tuple = (test[0],test[1],test[2])
 connection = mysql.connector.connect(host='localhost',database='icu',user='root',password='')
 cursor = connection.cursor()
-recordTuple = response
-cursor.execute("INSERT INTO audiodata (transcript, success, error) VALUES (%s,%s,%s)",recordTuple)
+recordTuple = return_tuple
+print(recordTuple)
+cursor.execute("""INSERT INTO audiodata (transcript, success, error) VALUES (%s,%s,%s);""",recordTuple)
 connection.commit()
 print(cursor.rowcount, "Record inserted successfully into database")
 cursor.close()
